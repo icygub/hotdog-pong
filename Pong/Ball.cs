1f151bca802dc35018bc0b93ad47770d1c1030a3
icygub <jchubbs95@gmail.com>
2018-04-06 23:21:57 -0600

    dog now spins

--- Pong/Ball.cs
+++ Pong/Ball.cs
@@ -8,17 +8,13 @@ namespace Pong
     public class Ball : Sprite
     {
 
-        private Paddle attachedToPaddle;
+        private Paddle _attachedToPaddle;
         public Vector2 Origin;
-
-        public Ball(Texture2D texture, Vector2 location) : base(texture, location)
-        {
-            //attachedToPaddle = null;
-        }
+        private float _rotation;
 
         public Ball(Texture2D texture, Vector2 location, Rectangle gameBoundaries) : base(texture, location, gameBoundaries)
         {
-            //attachedToPaddle = null;
+            _rotation = 0f;
         }
 
         protected override void CheckBounds()
@@ -38,18 +34,18 @@ namespace Pong
         public override void Update(GameTime gameTime, GameObjects gameObjects)
         {
             // fires the "ball"
-            if (gameObjects.TouchInput.Tap && attachedToPaddle != null)
+            if (gameObjects.TouchInput.Tap && _attachedToPaddle != null)
             {
-                var newVelocity = new Vector2(15f, attachedToPaddle.Velocity.Y * 1.2f);
+                var newVelocity = new Vector2(15f, _attachedToPaddle.Velocity.Y * 1.2f);
                 Velocity = newVelocity;
-                attachedToPaddle = null;
+                _attachedToPaddle = null;
             }
 
             // keeps "ball" attached to "paddle" while ball has not been fired
-            else if (attachedToPaddle != null)
+            else if (_attachedToPaddle != null)
             {
-                Location.Y = attachedToPaddle.Location.Y;
-                Location.X = attachedToPaddle.Location.X + this.Height;
+                Location.Y = _attachedToPaddle.Location.Y + _attachedToPaddle.Height/2;
+                Location.X = _attachedToPaddle.Location.X + this.Height;
             }         
 
             // this makes the ball deflect off the sides of the paddles
@@ -58,12 +54,18 @@ namespace Pong
                 Velocity = new Vector2(-Velocity.X, Velocity.Y);              
             }
 
+            _rotation += .04f;
             base.Update(gameTime, gameObjects);
         }
 
         public void AttachTo(Paddle paddle)
         {          
-            attachedToPaddle = paddle;
+            _attachedToPaddle = paddle;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch) {
+            //spriteBatch.Draw(texture, Location); // without spinning
+            spriteBatch.Draw(this.texture, Location, null, Color.White, _rotation, Origin, 1, SpriteEffects.None, 0f); // with spinning
         }
     }
 }
